7a88d02bdc4b2f9a1126954aeb282dc4e2b72908
JoakimMansson <jockel121.121@gmail.com>
2023-12-16 23:35:45 +0100

    implement gradient descent next

---
+++ Linear-Regression/Linear-Regression.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def linear_regression_MSE(data_x, data_y):
+    A = np.vstack([data_x, np.ones(len(data_x))])
+    A_T = A.T
+    A_inv = np.linalg.inv(np.dot(A, A_T))
+
+    k, m = np.dot(np.dot(A_inv, A), data_y)
+    print(f"k: {k}, m: {m}")
+    return k, m
+    
+if __name__ == "__main__":
+    data_x = np.array([0, 1, 2])
+    data_y = np.array([1, 3, 2])
+
+    k, m = linear_regression_MSE(data_x, data_y)
